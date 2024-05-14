19954f9d8009b99759c302764fa1f1b0e2d9d29e
Scyn4pse <canquechiara76@gmail.com>
2024-05-15 00:46:53 +0800

    Revised UI

--- app/src/main/java/com/donate/healthshapers/Profile.kt
+++ app/src/main/java/com/donate/healthshapers/Profile.kt
@@ -3,6 +3,7 @@ package com.donate.healthshapers
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 
 class Profile : AppCompatActivity() {
@@ -24,6 +25,25 @@ class Profile : AppCompatActivity() {
         val logoutButton = findViewById<LinearLayout>(R.id.logout)
         logoutButton.setOnClickListener{
             val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+        }
+
+        val dbBtn = findViewById<Button>(R.id.homebtn)
+        dbBtn.setOnClickListener{
+            val intent = Intent(this, RestaurantFrontPage::class.java)
+            startActivity(intent)
+        }
+
+        val dlButton = findViewById<Button>(R.id.donation_list_button)
+        dlButton.setOnClickListener{
+            val intent = Intent(this, Available_donations::class.java)
+            startActivity(intent)
+
+
+        }
+        val pfBtn = findViewById<Button>(R.id.iconButton)
+        pfBtn.setOnClickListener{
+            val intent = Intent(this, Profile::class.java)
             startActivity(intent)
         }
     }
