bb8a305ebe72bff9c5b0e98f6bd942cb7464068c
maxbeat9 <dominicrestauro@gmail.com>
2024-05-20 13:16:17 +0800

    Firebase Logout working

--- app/src/main/java/com/donate/healthshapers/Settings.kt
+++ app/src/main/java/com/donate/healthshapers/Settings.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import android.widget.Button
 import android.widget.LinearLayout
+import com.google.firebase.auth.FirebaseAuth
 
 class Settings : Fragment(R.layout.fragment_settings) {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
 
         // Initialize views and set click listeners
         val abtUsBtn = view.findViewById<LinearLayout>(R.id.about)
@@ -27,9 +32,13 @@ class Settings : Fragment(R.layout.fragment_settings) {
 
         val logoutButton = view.findViewById<LinearLayout>(R.id.logout)
         logoutButton.setOnClickListener {
+            // Sign out the user from Firebase
+            auth.signOut()
+
+            // Redirect to the login screen
             val intent = Intent(activity, SigninActivity::class.java)
             startActivity(intent)
+            activity?.finish()
         }
-
     }
 }
