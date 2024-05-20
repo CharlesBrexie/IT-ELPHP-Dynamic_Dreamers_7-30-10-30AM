42352157e412502fc8e008a09b599516177017a3
maxbeat9 <dominicrestauro@gmail.com>
2024-05-20 15:31:49 +0800

    Added unversioned Files

---
+++ app/src/main/java/com/donate/healthshapers/NgoFrontPage.kt
@@ -0,0 +1,15 @@
+package com.donate.healthshapers
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class NgoFrontPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ngo_front_page)
+
+        
+    }
+}
